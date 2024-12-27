ae7bb8ea7130520e5b95e5999bd039e10718e936
Arjun Sharma <107235667+arj5@users.noreply.github.com>
2024-12-27 09:19:05 -0500

    secret tic tac toe???

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,8 +5,10 @@ import { SOCIAL } from "../info";
 const Footer = () => {
   return (
     <div className="mb-8 mt-20">
-      <div className="flex items-center justify-center">
-        <img src={signW} width={200} alt="signW" className="my-20" />
+      <div className="flex items-center justify-center transition-transform duration-300 hover:scale-110">
+        <a href="https://smpl-tictactoe.netlify.app/" target="_blank">
+          <img src={signW} width={200} alt="signW" className="my-20" />
+        </a>
       </div>
       <div className="flex items-center justify-center gap-8">
         {SOCIAL.map((link, index) => (
